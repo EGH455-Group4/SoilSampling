83906a8e9fe0c2ceca575b3183fa439fb23bfb9d
Tom Bowyer <tom4310@gmail.com>
2023-09-23 07:28:32 +1000

    Add hardware calls

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # SoilSampling
 
+### Python commands
+
+python3 main.py
+
+### Virtual env commands
+
+python3 -m venv ss
+
+source ss/bin/activate
+
+deactivate
+
+### Python install commands
+
+pip3 install -r requirements.txt
+
+pip3 freeze > requirements.txt
+
 ### Install
 
 python3 -m venv ss
@@ -10,4 +28,6 @@ pip3 install -r requirements.txt
 
 can then test with python3 main.py
 
-### Run
+### Curl command
+
+curl -X POST http://127.0.0.1:8070/sample
--- motor/hardware_motor.py
+++ motor/hardware_motor.py
@@ -1,21 +1,44 @@
 '''Will hold information belonging to the hardware motor.'''
 import logging
-import time
 from threading import Lock
 
+from time import sleep
+
 from motor.motor import Motor
+from models.constants import MOTOR_GPIO_PIN, T
 
 class HardwareMotor(Motor):
     '''Implements the Motor class, and connects to hardware.'''
     def __init__(self):
+        from gpiozero import Servo
+
+        self.servo = Servo(MOTOR_GPIO_PIN)
+
         logging.info("Hardware motor setup")
 
     def sample(self, lock: Lock):
-        '''Will just log out the call and wait.'''
+        '''Will do the sampling process.'''
         logging.info("Sampling process called")
 
-        # Actually implement this function
-        time.sleep(5)
+        for _ in range(T):
+            self.servo.min()
+            logging.info("Minimum Pos")
+            sleep(1000)
+            self.servo.mid()
+            logging.info("Medium Pos")
+            sleep(1000)
+            self.servo.max()
+            logging.info("Maximum Pos")
+            sleep(1000)
+        for _ in range(T):
+            self.servo.max()
+            logging.info("Max Pos")
+            sleep(1000)
+            self.servo.mid()
+            logging.info("Mid Pos")
+            sleep(1000)
+            self.servo.min()
+            logging.info("Min Pos")
 
         logging.info("Done sampling")
 
