014f94c3e421e79dc28cf49cb791a102a42a621d
Tom Bowyer <tom4310@gmail.com>
2023-10-12 16:35:41 +1000

    Small changes

--- motor/hardware_motor.py
+++ motor/hardware_motor.py
@@ -5,24 +5,24 @@ from threading import Lock
 from time import sleep
 
 from motor.motor import Motor
-from models.constants import MOTOR_GPIO_PIN, T, SLEEP_INTERVAL
+from models.constants import MOTOR_GPIO_PIN
 
 class HardwareMotor(Motor):
     '''Implements the Motor class, and connects to hardware.'''
     def __init__(self):
-        from gpiozero import Servo
-
-        self.servo = Servo(MOTOR_GPIO_PIN)
-
         logging.info("Hardware motor setup")
-
-        self.servo.value = None
 
     def sample(self, lock: Lock):
         '''Will do the sampling process.'''
+        from gpiozero import Servo
+
         logging.info("Sampling process called")
 
-        
+        self.servo = Servo(MOTOR_GPIO_PIN)
+
+        logging.info("Hardware motor initialised")
+
+        self.servo.value = None
 
         self.servo.value = 1
         sleep(8)
@@ -30,29 +30,6 @@ class HardwareMotor(Motor):
         sleep(10)
         self.servo.value = None
         sleep(1)
-        
-        
-        
-        
-        '''for _ in range(T):
-            self.servo.max()
-            logging.info("Minimum Pos")
-            sleep(SLEEP_INTERVAL)
-            self.servo.mid()
-            logging.info("Medium Pos")
-            sleep(SLEEP_INTERVAL)
-            self.servo.min()
-            logging.info("Maximum Pos")
-            sleep(SLEEP_INTERVAL)'''
-        '''for _ in range(T):
-            self.servo.max()
-            logging.info("Max Pos")
-            sleep(SLEEP_INTERVAL)
-            self.servo.mid()
-            logging.info("Mid Pos")
-            sleep(SLEEP_INTERVAL)
-            self.servo.min()
-            logging.info("Min Pos")'''
 
         logging.info("Done sampling")
 
